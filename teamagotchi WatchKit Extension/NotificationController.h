137379469ef0d027970140586083016ad8d08727
lillyfwang <lillyfwang@yahoo.com>
2015-06-26 22:44:46 -0700

    added watchkit app files and setup

---
+++ teamagotchi WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  teamagotchi WatchKit Extension
+//
+//  Created by Lilly Wang on 6/26/15.
+//  Copyright (c) 2015 Lilly Wang. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
