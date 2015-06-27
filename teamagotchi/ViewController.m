a2ff6ed3fb63fce2331893d3a4f9bdefb0ac3006
lillyfwang <lillyfwang@yahoo.com>
2015-06-26 22:38:18 -0700

    Initial Commit

---
+++ teamagotchi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  teamagotchi
+//
+//  Created by Lilly Wang on 6/26/15.
+//  Copyright (c) 2015 Lilly Wang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
